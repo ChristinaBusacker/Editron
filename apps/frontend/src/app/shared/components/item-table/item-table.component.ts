5828869e2d1eeb7c31cdd59393702a88d8638795
Christina <email@christina-busacker.de>
2025-06-23 11:25:29 +0200

    improve color field on table

--- apps/frontend/src/app/shared/components/item-table/item-table.component.ts
+++ apps/frontend/src/app/shared/components/item-table/item-table.component.ts
@@ -157,6 +157,23 @@ export class ItemTableComponent implements AfterViewInit {
     };
   }
 
+  getContrastColor(hex: string): string {
+    if (!hex) return '#000';
+    hex = hex.replace('#', '');
+    if (hex.length === 3) {
+      hex = hex
+        .split('')
+        .map(c => c + c)
+        .join('');
+    }
+
+    const r = parseInt(hex.substr(0, 2), 16);
+    const g = parseInt(hex.substr(2, 2), 16);
+    const b = parseInt(hex.substr(4, 2), 16);
+    const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+    return brightness > 128 ? '#000000' : '#FFFFFF';
+  }
+
   readFieldValue(field: FieldDefinition, element: any) {
     const content = element.content[field.name];
 
